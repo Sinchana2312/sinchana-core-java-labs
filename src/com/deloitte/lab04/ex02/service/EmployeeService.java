a6fc9343d8feb2b627bdee6f004bf23728b471b4
Sinchana2312 <115571946+Sinchana2312@users.noreply.github.com>
2024-09-25 21:20:19 +0530

    Delete src/com/deloitte/lab04/ex02/service directory

--- src/com/deloitte/lab04/ex02/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.deloitte.lab04.ex02.service;
-
-import com.deloitte.lab04.ex02.bean.Employee;
-
-public interface EmployeeService {
-    Employee getEmployeeDetails();
-    void displayEmployeeDetails(Employee employee);
-}
